37b84b641bdc6dc7effcdb229563ef2b1ef8a245
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-02-06 18:16:16 +0600

    login added
    login added

--- barong-api-user-auth-new/src/components/Login.js
+++ barong-api-user-auth-new/src/components/Login.js
@@ -1,7 +1,45 @@
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+// import { useHistory } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+
+
+    //  const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
+
+    // const navigate = useNavigate();
+    // const history = useHistory();
+    useEffect(() => {
+        if (localStorage.getItem('user-info')) {
+            // history.push("/add")
+            // navigate('/product');
+        }
+    }, [navigate]);
+    async function login() {
+        console.warn(email, password)
+        let item = { email, password }
+        let result = await fetch("https://cp.btfd.cc/api/v2/barong/identity/users",
+
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(item)
+            });
+        result = await result.json()
+        console.warn("result", result)
+        localStorage.setItem("user-info", JSON.stringify(result))
+        // navigate('/product');
+
+
+    }
     return (
 
         <div className="col-sm-4 offset-sm-4 resgister-div">
@@ -15,14 +53,15 @@ const Login = () => {
 
 
 
-
-                <input type="email" className="form-control" placeholder="Email" />
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Email" />
                 <br />
 
-                <input type="password" className="form-control" placeholder="Password" />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Password" />
                 <br />
 
-                <button className="btn btn-sign">Login</button>
+                <button onClick={login} className="btn btn-sign">Login</button>
+
+
             </div>
 
         </div>
