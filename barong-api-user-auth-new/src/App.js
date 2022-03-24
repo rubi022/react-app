32a936cc48af2a6314022d82e59d3e82fca33472
Hasham-dev <vakanihasham64@gmail.com>
2022-03-24 18:50:49 +0500

    feat: on load fetch user saved session from LS

--- barong-api-user-auth-new/src/App.js
+++ barong-api-user-auth-new/src/App.js
@@ -1,23 +1,36 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Register from './components/Register';
-import Login from './components/Login';
-import LandingPage from './components/LandingPage';
-import VerifyEmail from './components/VerifyEmail';
-
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import LandingPage from "./components/LandingPage";
+import VerifyEmail from "./components/VerifyEmail";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState();
+  const userDetails = JSON.parse(localStorage.getItem("user-info"));
+
+  const loadUser = useCallback(() => {
+    setUser(userDetails);
+  }, []);
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar setUser={setUser} user={user} />
       <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route path="email-verification" element={<VerifyEmail />} />
-
+        <Route exact path="/" element={<LandingPage user={user} />} />
+        <Route path="register" element={<Register user={user} />} />
+        <Route path="login" element={<Login user={user} setUser={setUser} />} />
+        <Route
+          path="email-verification"
+          element={<VerifyEmail user={user} />}
+        />
       </Routes>
     </div>
   );
