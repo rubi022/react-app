7b07049dfb5f8bcdbc108e93aa8b8402aa4bc3bc
Hasham-dev <vakanihasham64@gmail.com>
2022-03-24 17:49:32 +0500

    fix: don't show login & signup buttons if user already logged in

--- barong-api-user-auth-new/src/components/Login.js
+++ barong-api-user-auth-new/src/components/Login.js
@@ -2,65 +2,77 @@ import { Link } from "react-router-dom";
 import React, { useState } from "react";
 // import React, { useState, useEffect } from "react";
 
-
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  // const navigate = useNavigate();
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    // const navigate = useNavigate();
+  async function onSubmit(e) {
+    e.preventDefault();
 
+    let item = { email, password };
+    let result = await fetch(
+      "https://cp.btfd.cc/api/v2/barong/identity/sessions",
 
-    async function onSubmit(e) {
-        e.preventDefault();
-
-        let item = { email, password }
-        let result = await fetch("https://cp.btfd.cc/api/v2/barong/identity/sessions",
-
-            {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify(item)
-            });
-        result = await result.json()
-        console.warn("result", result)
-        localStorage.setItem("user-info", JSON.stringify(result))
-        // navigate('/product');
-
-
-    }
-    return (
-
-        <div className="col-sm-4 offset-sm-4 resgister-div">
-            <div className="card card-sign">
-                <div className="btn-group">
-                    <Link to="/register" className="btn btn-group-top-reg active" aria-current="page">SIGN UP</Link>
-                    <Link to="/login" className="btn btn-group-top-log ">SIGN IN</Link>
-
-                </div>
-                <br />
-
-                <form action="" className="login-form" onSubmit={onSubmit}>
-
-
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Email" required />
-                    <br />
-
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Password" required />
-                    <br />
-
-                    {/* <button onClick={login} className="btn btn-sign">Login</button> */}
-                    <input type="submit" value="Login" className="btn btn-sign" />
-
-
-                </form>
-            </div>
-
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(item),
+      }
+    );
+    result = await result.json();
+    console.warn("result", result);
+    localStorage.setItem("user-info", JSON.stringify(result));
+    // navigate('/product');
+  }
+  return (
+    <div className="col-sm-4 offset-sm-4 resgister-div">
+      <div className="card card-sign">
+        <div className="btn-group">
+          <Link
+            to="/register"
+            className="btn btn-group-top-reg active"
+            aria-current="page"
+          >
+            SIGN UP
+          </Link>
+          <Link to="/login" className="btn btn-group-top-log ">
+            SIGN IN
+          </Link>
         </div>
-    )
+        <br />
+
+        <form action="" className="login-form" onSubmit={onSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            placeholder="Email"
+            required
+          />
+          <br />
+
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            placeholder="Password"
+            required
+          />
+          <br />
+
+          {/* <button onClick={login} className="btn btn-sign">Login</button> */}
+          <input type="submit" value="Login" className="btn btn-sign" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
