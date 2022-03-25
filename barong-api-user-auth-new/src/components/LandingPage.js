1d574efa2c609933247f50714ea38be36987b531
Hasham-dev <vakanihasham64@gmail.com>
2022-03-25 11:32:14 +0500

    feat: render user uid in landing page

--- barong-api-user-auth-new/src/components/LandingPage.js
+++ barong-api-user-auth-new/src/components/LandingPage.js
@@ -4,12 +4,14 @@ const LandingPage = ({ user }) => {
   //   const userDetails = JSON.parse(localStorage.getItem("user-info"));
   if (!user) return <Navigate to="/login" />;
 
-  const { email, state, role } = user;
+  const { email, state, role, uid } = user;
+
   return (
     <div className="landing-div" style={{ textAlign: "center" }}>
       <h3>Landing page</h3>
       <br />
       <h3>User Email: {email}</h3>
+      <h3>User UID: {uid}</h3>
       <h3>State: {state}</h3>
       <h3>Role: {role}</h3>
     </div>
